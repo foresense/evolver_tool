9850fb5db5dea5dc2a07bcb06654c599fba26b7a
Robert Beenen <robert@foresense.org>
2022-03-01 18:16:07 +0100

    getting somewhere

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from queue import Queue
+from threading import Thread
+from time import sleep
+
 import mido
 
 
-# Evolver Sysex id
+OUT_SPEED = 1 / 10         # 10 messages per second out rate
 
 SYSEX_ID = (0x01, 0x20, 0x01)
 
@@ -18,6 +22,8 @@ NAME_DUMP = 0x11
 PROG_REQ = 0x05
 EDIT_REQ = 0x06
 WAVE_REQ = 0x0B
+MAIN_REQ = 0x0E
+NAME_REQ = 0x10
 
 RESET = 0x04
 START_STOP = 0x12
@@ -25,44 +31,89 @@ SHIFT_ON = 0x13
 SHIFT_OFF = 0x14
 
 
-def pack(parameters: list) -> list:
-    """pack 7x 8-bit bytes into 8x 7-bit bytes"""
+main_parameters = (
+    "program", "bank", "volume", "transpose", "bpm", "clock_div", "program_tempo", "midi_clock",
+    "lock_seq", "poly_chain", "input_gain", "fine_tune", "midi_rec", "midi_xmit", "midi_chan", "midi_dump"
+)
 
+
+#program_number = None
+#bank_number = None
+
+queue_out = Queue(maxsize=128)
+
+main_memory = {}
+edit_memory = {}
+
+
+def pack1111(parameters: list) -> list:
     byte = 0
     out = []
-
     for n, val in enumerate(parameters):
         high, low = divmod(val, 1 << 7)
         byte = byte | high << n
         out.append(low)
         # print(f"{high=} {low=}, {bin(byte)=}, {n=}")
-
     return [byte] + out
 
 
-def ms_split(parameter: int) -> tuple:
-    pass
+def pack_data(data: dict) -> list:
+    packed = [0]
+    for val in reversed(data):
+        packed[0] = packed[0] << 1 | val >> 7
+        packed.append(val & 0x7f)
+    return packed
 
 
-def pack(data: tuple) -> tuple:
-    pass
+def unpack_data(packed: tuple):
+   
+    if packed[0] not in (PROG_PAR, SEQ_PAR, MAIN_PAR, PROG_DUMP, EDIT_DUMP, WAVE_DUMP, MAIN_DUMP, NAME_DUMP):
+        print(f"what? {packed}")
+    # alternate LS then MS byte
+    if packed[0] == MAIN_DUMP:
+        print(f"{len(packed[1:]} bytes: packed[1:]))
+        for n in packed[1:]:
+            pass
+            #algorithm that makes LS, MS pairs into one byte
 
 
-
-def unpack(packed):
-    """unpack 8 7-bit bytes into 7 8-bit bytes"""
-
-    byte, out = packed[0], packed[1:]
-
-    for n, val in enumerate(out):
-
-        pass
+def queue_message(data: list) -> mido.Message:
+    queue_out.put(mido.Message(type='sysex', data=(*SYSEX_ID, *data)))
 
 
-test_input = [0xC0, 0xA0, 0x90, 0x88, 0x82, 0x84, 0x82]
-test_packed = pack(test_input)  # [64, 32, 16, 8, 2, 4, 2, 0]
+def midi_in_callback(message: mido.Message):
+    if message.type == 'program_change':
+        # print(f"program number: {message.program + 1}")
+        main_memory.update({"program_number": message.program})
+        queue_message([EDIT_REQ])
+    
+    elif message.type == 'control_change' and message.control == 32:
+        # print(f"bank number: {m.value + 1}")
+        main_memory.update({"bank_number": message.value})
+        queue_message([EDIT_REQ])
+    
+    elif message.type == 'sysex' and message.data[:3] == SYSEX_ID:
+        data = unpack_data(message.data[3:])
+    
+    else:
+        print(message.data)
+        
 
-midi_in = mido.open_input('MidiKliK 1')
-midi_out = mido.open_output('MidiKliK 2')
 
-dump_in = mido.Message(type='sysex', data=(1, 32, 1, 2, 1, 20, 0, 36, 45, 1, 100, 48, 80, 1, 0, 100, 24, 50, 0, 33, 36, 50, 0, 0, 33, 88, 117, 0, 33, 50, 0, 33, 0, 0, 0, 100, 0, 23, 0, 50, 67, 0, 100, 0, 33, 0, 0, 112, 33, 100, 0, 0, 3, 0, 0, 7, 31, 80, 1, 23, 20, 99, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 2, 3, 12, 0, 100, 0, 0, 6, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 3, 4, 0, 0, 0, 2, 4, 0, 1, 0, 27, 0, 0, 0, 4, 111, 20, 0, 0, 0, 64, 99, 0, 0, 99, 0, 3, 70, 0, 4, 33, 0, 0, 0, 108, 9, 0, 0, 50, 39, 23, 0, 0, 0, 0, 0, 0, 99, 0, 116, 10, 99, 0, 0, 99, 0, 99, 0, 99, 0, 0, 99, 0, 96, 0, 0, 68, 0, 0, 0, 74, 73, 96, 0, 0, 74, 0, 0, 86, 68, 72, 62, 24, 38, 0, 0, 0, 48, 32, 101, 62, 64, 0, 58, 45, 44, 43, 101, 50, 48, 0, 0, 0, 32, 0, 0, 24, 16, 0, 54, 24, 10, 32, 0, 16, 48, 0, 40, 38, 24, 16, 17, 18, 19, 0, 20, 21, 24, 28, 18, 16, 101, 0, 0, 0, 0))
+def queue_out_thread():
+    while(not midi_out.closed):
+        if not queue_out.empty():
+            midi_out.send(queue_out.get())
+            queue_out.task_done()
+        sleep(OUT_SPEED)
+    print("bye!")
+
+
+if __name__ == "__main__":
+    midi_in = mido.open_input('MidiKliK 1', callback=midi_in_callback)
+    midi_out = mido.open_output('MidiKliK 2')
+
+    t = Thread(target=queue_out_thread)
+    t.start()
+
+    queue_message([MAIN_REQ])
